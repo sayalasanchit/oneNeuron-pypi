20e6d7d98441b365ecaddbeaca7d8a88a3ccf65e
sayalasanchit <sayalasanchit@gmail.com>
2021-09-22 23:50:04 +0530

    setup.py file corrected

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires={
+    install_requires=[
         "numpy",
         "tqdm"
-    }
+    ]
 )
